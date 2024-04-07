0dc33bb756f26c85791596128957e1a333578eeb
krystiank95 <krystiankroliczak1995@gmail.com>
2024-04-07 20:53:59 +0200

    create oop for object oriented programinig

--- javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
+++ javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from './checkouts/orderSummary.js'
 import { renderPaymentSummary } from './checkouts/paymentSummary.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
