a21276d9a4cc36e06e6ce8dd7e9e12ddd987543a
krystiank95 <krystiankroliczak1995@gmail.com>
2024-05-02 12:42:13 +0200

    Convert products into classes

--- javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
+++ javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from './checkouts/orderSummary.js'
 import { renderPaymentSummary } from './checkouts/paymentSummary.js';
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
