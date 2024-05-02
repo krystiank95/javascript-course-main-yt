184b2d6ef26c5fea0fc260c8b315a9b74e0ebaa3
krystiank95 <krystiankroliczak1995@gmail.com>
2024-05-02 12:13:58 +0200

    create classs version of cart

--- javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
+++ javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from './checkouts/orderSummary.js'
 import { renderPaymentSummary } from './checkouts/paymentSummary.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
